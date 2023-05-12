2ba1415107f23908805068fe41b7c1fa72bd2e47
Sergiu Nagailic <nikro.md@gmail.com>
2023-05-12 19:49:23 +0200

    Initial commit

---
+++ MMM-WhisperGPT.js
@@ -0,0 +1,91 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-WhisperGPT
+ *
+ * By Sergiu Nagailic
+ * MIT Licensed.
+ */
+
+Module.register("MMM-WhisperGPT", {
+	defaults: {
+    state: 'idle',
+    text: 'Shhh! WhisperGPT is listening...'
+	},
+
+	requiresVersion: "2.1.0", // Required version of MagicMirror
+
+	start: function() {
+    Log.info("Starting module: " + this.name);
+	},
+
+  getDom: function() {
+    var wrapper = document.createElement("div");
+
+    // State-based UI rendering
+    switch(this.state) {
+      case 'idle':
+        wrapper.innerHTML = "Waiting for trigger word...";
+        // TODO: Display service status indicator
+        break;
+      case 'listening':
+        wrapper.innerHTML = "Listening...";
+        // TODO: Display audio visualization widget
+        break;
+      case 'processing':
+        wrapper.innerHTML = "Processing...";
+        // TODO: Display processing indicator
+        break;
+      case 'speaking':
+        wrapper.innerHTML = this.config.text;
+        // TODO: Display textual reply and start text-to-speech
+        break;
+      default:
+        wrapper.innerHTML = "Unknown state";
+        break;
+    }
+
+    return wrapper;
+  },
+
+	getScripts: function() {
+		return [];
+	},
+
+	getStyles: function () {
+		return [
+			"MMM-WhisperGPT.css",
+		];
+	},
+
+	// Load translations files
+	getTranslations: function() {
+		//FIXME: This can be load a one file javascript definition
+		return {
+			en: "translations/en.json",
+			es: "translations/es.json"
+		};
+	},
+
+	processData: function(data) {
+		var self = this;
+		this.dataRequest = data;
+		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
+		this.loaded = true;
+
+		// the data if load
+		// send notification to helper
+		this.sendSocketNotification("MMM-WhisperGPT-NOTIFICATION_TEST", data);
+	},
+
+	// socketNotificationReceived from helper
+	socketNotificationReceived: function (notification, payload) {
+		if(notification === "MMM-WhisperGPT-NOTIFICATION_TEST") {
+			// set dataNotification
+			this.dataNotification = payload;
+			this.updateDom();
+		}
+	},
+});
+
+
---
+++ node_helper.js
@@ -0,0 +1,47 @@
+/* Magic Mirror
+ * Node Helper: MMM-WhisperGPT
+ *
+ * By Sergiu Nagailic
+ * MIT Licensed.
+ */
+
+var NodeHelper = require("node_helper");
+
+module.exports = NodeHelper.create({
+
+	// Override socketNotificationReceived method.
+
+	/* socketNotificationReceived(notification, payload)
+	 * This method is called when a socket notification arrives.
+	 *
+	 * argument notification string - The identifier of the noitication.
+	 * argument payload mixed - The payload of the notification.
+	 */
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === "MMM-WhisperGPT-NOTIFICATION_TEST") {
+			console.log("Working notification system. Notification:", notification, "payload: ", payload);
+			// Send notification
+			this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
+		}
+	},
+
+	// Example function send notification test
+	sendNotificationTest: function(payload) {
+		this.sendSocketNotification("MMM-WhisperGPT-NOTIFICATION_TEST", payload);
+	},
+
+	// this you can create extra routes for your module
+	extraRoutes: function() {
+		var self = this;
+		this.expressApp.get("/MMM-WhisperGPT/extra_route", function(req, res) {
+			// call another function
+			values = self.anotherFunction();
+			res.send(values);
+		});
+	},
+
+	// Test another function
+	anotherFunction: function() {
+		return {date: new Date()};
+	}
+});
