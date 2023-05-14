0b96bfda74f7e09130d80e38d9a649028b5d1817
Sergiu Nagailic <nikro.md@gmail.com>
2023-05-14 02:50:31 +0200

    Adjusting the detection - still not working.

--- node_helper.js
+++ node_helper.js
@@ -34,19 +34,28 @@ module.exports = NodeHelper.create({
       fileType: 'wav'
     });
 
-    const micInputStream = micInstance.getAudioStream();
+    let buffer = [];
+    micInstance.getAudioStream().on('data', (data) => {
+      // Append new data to the buffer
+      buffer = buffer.concat(Array.from(data));
+
+      // If buffer is long enough, process it and remove used data
+      while (buffer.length >= porcupine.frameLength) {
+        const frame = buffer.slice(0, porcupine.frameLength);
+        buffer = buffer.slice(porcupine.frameLength);
+
+        const keywordIndex = porcupine.process(frame);
+        if (keywordIndex >= 0) {
+          this.sendSocketNotification('KEYWORD_DETECTED', keywordIndex);
+        }
+      }
+    });
+
     const porcupine = new Porcupine(
       this.config.picovoiceKey,
       [BuiltinKeyword[this.config.picovoiceWord]],
       [0.65]
     );
-
-    micInputStream.on('data', (audioFrame) => {
-      const keywordIndex = porcupine.process(audioFrame);
-      if (keywordIndex >= 0) {
-        this.sendSocketNotification('KEYWORD_DETECTED', keywordIndex);
-      }
-    });
 
     micInstance.start();
   },
